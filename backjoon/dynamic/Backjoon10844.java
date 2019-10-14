9bec95a13483da25eefd03e8721c711aa62c57c5
kimyonghee <yhk06262@naver.com>
2019-10-14 23:48:50 +0900

    algorithm at part 'Dynamic Programming'

--- backjoon/dynamic/Backjoon10844.java
+++ backjoon/dynamic/Backjoon10844.java
@@ -1,4 +1,20 @@
 package backjoon.dynamic;
 
+import java.io.*;
+
 public class Backjoon10844 {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        long answer = 9;
+        while(true){
+            if(--n == 0) break;
+            answer = answer * 2 - 1;
+        }
+        bw.write(answer + "\n");
+        bw.close();
+        br.close();
+    }
 }
--- backjoon/dynamic/Backjoon1463.java
+++ backjoon/dynamic/Backjoon1463.java
@@ -5,10 +5,24 @@ import java.io.*;
 public class Backjoon1463 {
     private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+    private static long dp[];
     public static void main(String[] args) throws IOException {
         int n = Integer.parseInt(br.readLine());
-
+        long min;
+        dp = new long[n + 1];
+        dp[0] = 0;
+        dp[1] = 0;
+        for(int i = 2 ; i <= n; i++){
+            dp[i] = dp[i - 1] + 1;
+            if(i % 3 == 0) dp[i] = min(dp[i], dp[i / 3] + 1);
+            if(i % 2 == 0) dp[i] = min(dp[i], dp[i / 2] + 1);
+        }
+        bw.write(dp[n] + "\n");
+        bw.close();
+        br.close();
+    }
+    private static long min(long a, long b){
+        return a > b ? b : a;
     }
 
 }
