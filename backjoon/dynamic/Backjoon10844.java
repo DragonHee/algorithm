11ab53764d47c817e6d252aade055354f494974b
kimyonghee <yhk06262@naver.com>
2019-10-15 23:37:33 +0900

    algorithm at part 'Dynamic Programming'

--- backjoon/dynamic/Backjoon10844.java
+++ backjoon/dynamic/Backjoon10844.java
@@ -5,15 +5,29 @@ import java.io.*;
 public class Backjoon10844 {
     private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static long dp[][];
+    private static final int NUM = 1_000_000_000;
 
     public static void main(String[] args) throws IOException {
         int n = Integer.parseInt(br.readLine());
-        long answer = 9;
-        while(true){
-            if(--n == 0) break;
-            answer = answer * 2 - 1;
+        dp = new long[n + 1][10];
+        long answer = 0;
+        for(int i = 1; i <= 9; i++){
+            dp[1][i] = 1;
         }
-        bw.write(answer + "\n");
+
+        for(int i = 2; i <= n; i++){
+           for(int j = 0; j <= 9; j++){
+               if(j == 0) dp[i][j] = dp[i - 1][j + 1] % NUM;
+               else if(j >= 1 && j <= 8) dp[i][j] = dp[i - 1][j - 1] % NUM + dp[i - 1][j + 1]  % NUM;
+               else dp[i][j] = dp[i - 1][j - 1] % NUM;
+           }
+        }
+
+        for(int i = 0 ; i <= 9; i++){
+            answer =  (answer + dp[n][i]) % NUM;
+        }
+        bw.write(answer % 1_000_000_000 + "\n");
         bw.close();
         br.close();
     }
