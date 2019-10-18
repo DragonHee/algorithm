cdfafbb7dbe1fffcf889703c16b5785a487077aa
kimyonghee <yhk06262@naver.com>
2019-10-18 21:27:37 +0900

    algorithm at part 'Dynamic Programming'

---
+++ backjoon/dynamic/Backjoon9251.java
@@ -0,0 +1,17 @@
+package backjoon.dynamic;
+
+import java.io.*;
+
+public class Backjoon9251 {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+
+        
+        bw.close();
+        br.close();
+    }
+}
