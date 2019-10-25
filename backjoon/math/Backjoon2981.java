40ef3aa4690e274485dd9dc8fa5b15a1d1fd3973
kimyonghee <yhk06262@naver.com>
2019-10-26 00:44:05 +0900

    algorithm at part 'basic math'

--- backjoon/math/Backjoon2981.java
+++ backjoon/math/Backjoon2981.java
@@ -3,6 +3,7 @@ package backjoon.math;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Backjoon2981 {
@@ -11,13 +12,36 @@ public class Backjoon2981 {
 
     public static void main(String[] args) throws IOException {
         int n = Integer.parseInt(br.readLine());
+        List<Integer> list = new ArrayList<>();
         int arr[] = new int[n];
-        List<Integer> list = new ArrayList<Integer>();
 
         for(int i = 0; i < n; i++) arr[i] = Integer.parseInt(br.readLine());
-
         Arrays.sort(arr);
-        for
 
+        int gcdNum = arr[1] - arr[0];
+
+        for(int i = 2 ; i < n; i++){
+            gcdNum = gcd(gcdNum, arr[i] - arr[i - 1]);
+        }
+        for(int i = 2; i * i <= gcdNum; i++){
+            if(gcdNum % i == 0) {
+                list.add(i);
+                if(i != gcdNum / i) list.add(gcdNum / i);
+            }
+        }
+        list.add(gcdNum);
+        Collections.sort(list);
+        for(int i = 0 ; i < list.size(); i++) bw.write(list.get(i) + " ");
+        bw.write("\n");
+        bw.close();
+        br.close();
+    }
+    public static int gcd(int a, int b){
+        while(b != 0){
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
     }
 }
