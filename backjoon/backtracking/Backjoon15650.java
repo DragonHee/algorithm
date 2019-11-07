6e2e71a3f3d0c8f95f1b69c4ba95588d6eee48d1
kimyonghee <yhk06262@naver.com>
2019-11-07 13:02:26 +0900

    전형적인 백트래킹 문제(재귀를 이용한 수열 찾기) - DFS
    1. 총 길이가 입력된 길이와 같을 경우 출력한다.
    2. 1이 아닌경우 수열에 값을 입력하고, 재귀함수를 호출하여 다음 수열에 값을 입력한다.

---
+++ backjoon/backtracking/Backjoon15650.java
@@ -0,0 +1,35 @@
+package backjoon.backtracking;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Backjoon15650 {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static int m, n;
+    private static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[m + 1];
+        solve(1);
+        bw.close();
+        br.close();
+    }
+
+    public static void solve(int cnt) throws IOException {
+        if(cnt - 1 == m){
+            for(int i = 1; i <= m; i++) bw.write(arr[i] + " ");
+            bw.write("\n");
+            return;
+        }
+        for(int i = 1; i <= n; i++){
+            if(arr[cnt - 1] < i) {
+                arr[cnt] = i;
+                solve(cnt + 1);
+            }
+        }
+    }
+}
