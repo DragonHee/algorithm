fc3ccfe5a668c18d1483ce1405eeb25d53b3dd1a
kimyonghee <yhk06262@naver.com>
2019-11-09 18:33:22 +0900

    조세퍼스 순열 문제 (queue 메소드 쓸수 있으면 풀 수 있는 문제)
    * 앞에서 부터 빼고 뒤에 집어넣는(FIFO) 구조
    
    1. 입력된 값에 때라 출력한다.

--- backjoon/queuedeque/Backjoon11866.java
+++ backjoon/queuedeque/Backjoon11866.java
@@ -9,17 +9,34 @@ public class Backjoon11866 {
     private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     private static int[] arr;
     private static LinkedList<Integer> queue = new LinkedList<>();
-
+    private static int k;
     public static void main(String[] args) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
         arr = new int[n + 1];
         for(int i = 1; i <= n; i++) queue.push(i);
 
         solve(k);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+        for(int i = 1; i <= n; i++) {
+            if(i != n) sb.append(arr[i] + ", ");
+            else sb.append(arr[i] + ">");
+        }
+        bw.write(sb + "\n");
+        bw.close();
+        br.close();
     }
     private static void solve(int k){
-
+        int idx = 1;
+        while(!queue.isEmpty()){
+            for(int i = 0 ; i < k; i++){
+                int num = queue.pollLast();
+                if(i == k - 1) arr[idx++] = num;
+                else queue.push(num);
+            }
+        }
     }
 }
