a7ca706008859b4e6554038579afad2efa995542
kimyonghee <yhk06262@naver.com>
2019-11-14 23:15:05 +0900

    기본적인 분할 정복 문제

---
+++ backjoon/divideandconquer/Backjoon1629.java
@@ -0,0 +1,14 @@
+package backjoon.divideandconquer;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Backjoon1629 {
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+    }
+}
