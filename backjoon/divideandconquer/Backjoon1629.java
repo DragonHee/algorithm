ee2cbb4a8dc447c7a80e9fa67e272e18d18a39da
kimyonghee <yhk06262@naver.com>
2019-11-16 16:50:36 +0900

    분할 정복 문제
    * 거듭제곱이라는 형태를
    
    제곱수가 1. 짝수인 경우, 2. 홀수인 경우로 나누어 풀이 한다.
    a의 b승 일때 b번만큼 곱해주면 시간 복잡도가 너무나 크게 나옴.

--- backjoon/divideandconquer/Backjoon1629.java
+++ backjoon/divideandconquer/Backjoon1629.java
@@ -6,9 +6,27 @@ import java.util.StringTokenizer;
 public class Backjoon1629 {
     static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+    static int a, b, c;
     public static void main(String[] args) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
 
+        long ans = pow(a, b);
+        bw.write(ans + "\n");
+
+        bw.close();
+        br.close();
+    }
+    static long pow(int a, int b){
+        if(b == 0){
+            return 1;
+        }
+        long n = pow(a, b / 2);
+        long temp = n * n % c;
+
+        if(b % 2 == 0) return temp;
+        else return temp * a % c;
     }
 }
