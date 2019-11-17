482e6463f38b8242774d7c3d5e3095a3c4a995d0
kimyonghee <yhk06262@naver.com>
2019-11-17 21:21:16 +0900

    분할 정복 문제
    * 분할하여 이항계수를 푸는 문제

---
+++ backjoon/divideandconquer/Backjoon11401.java
@@ -0,0 +1,15 @@
+package backjoon.divideandconquer;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Backjoon11401 {
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) {
+
+    }
+}
