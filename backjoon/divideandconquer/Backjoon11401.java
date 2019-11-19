1f57ddb3c69f5586d2c8ec653e2d2b913a528151
kimyonghee <yhk06262@naver.com>
2019-11-19 23:35:23 +0900

    분할 정복 문제
    * 분할하여 이항계수를 푸는 문제

--- backjoon/divideandconquer/Backjoon11401.java
+++ backjoon/divideandconquer/Backjoon11401.java
@@ -1,17 +1,24 @@
 package backjoon.divideandconquer;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Backjoon11401 {
     static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
-
+        long ans = solve(n, k);
+        bw.write(ans + "\n");
+        bw.close();
+        br.close();
+    }
+    public static long solve(int n, int k){
+        return 1;
     }
 }
