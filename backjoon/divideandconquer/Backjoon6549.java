cda2bb4fbd35d25fd29772a95ee438bdcd0cd646
kimyonghee <yhk06262@naver.com>
2020-01-18 16:46:22 +0900

    [히스토그램 문제]
    방식은
    1. 세그먼트 + 분할 정복
    
     -- > 이해가 잘 안됨
    
    2. 스택을 이용
    
     -- > 스택을 이용한 풀이법이다.

--- backjoon/divideandconquer/Backjoon6549.java
+++ backjoon/divideandconquer/Backjoon6549.java
@@ -2,69 +2,37 @@ package backjoon.divideandconquer;
 
 
 import java.io.*;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
-class SegmentTree{
-    int n, arr[], rangeMinIndex[];
+class Info{
+    int idx;
+    long height;
 
-    public SegmentTree(int arr[]){
-        n = arr.length;
-        this.arr = arr.clone();
-        rangeMinIndex = new int[n << 2];
-
-        init(0, n - 1, 1);
+    public Info(int idx, long height) {
+        this.idx = idx;
+        this.height = height;
     }
 
-    private int init(int left, int right, int node){
-        if(left == right) {
-            return rangeMinIndex[node] = left;
-        }
-
-        int mid = (left + right) / 2;
-        int leftMinIndex = init(left, mid, node << 1);
-        int rightMinIndex = init(mid + 1, right, (node << 1) + 1);
-
-        return rangeMinIndex[node] = arr[leftMinIndex] < arr[rightMinIndex] ?
-                leftMinIndex : rightMinIndex;
-    }
-    private int query(int left, int right, int node, int nodeLeft, int nodeRight){
-        if(nodeRight < left || right < nodeLeft) return Integer.MAX_VALUE;
-        if(left <= nodeLeft && right >= nodeRight) return rangeMinIndex[node];
-
-        int mid = (nodeLeft + nodeRight) >>> 1;
-        int leftMinIndex = query(left, right, node << 1, nodeLeft, mid);
-        int rightMinIndex = query(left, right, (node << 1) + 1, mid + 1, nodeRight);
-
-        if(leftMinIndex == Integer.MAX_VALUE) return rightMinIndex;
-        else if(rightMinIndex == Integer.MAX_VALUE) return leftMinIndex;
-        else return arr[leftMinIndex] < arr[rightMinIndex] ? leftMinIndex : rightMinIndex;
-
-
+    public int getIdx() {
+        return idx;
     }
 
-    public long getMaxWidth(int left, int right){
-        long maxWidth, tmpWidth;
-        int minIndex = query(left, right, 1, 0, n - 1);
-
-        maxWidth = (long)(right - left + 1) * (long)arr[minIndex];
-
-        if(left <= minIndex - 1){
-            tmpWidth = getMaxWidth(left, minIndex - 1);
-            maxWidth = Math.max(maxWidth, tmpWidth);
-        }
-
-        if(minIndex + 1 <= right){
-            tmpWidth = getMaxWidth(minIndex + 1, right);
-            maxWidth = Math.max(maxWidth, tmpWidth);
-        }
-        return maxWidth;
+    public void setIdx(int idx) {
+        this.idx = idx;
     }
 
+    public long getHeight() {
+        return height;
+    }
+
+    public void setHeight(long height) {
+        this.height = height;
+    }
 }
 public class Backjoon6549 {
     private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static int[] arr;
+    static long[] arr;
     static int n;
     public static void main(String[] args) throws IOException {
         StringTokenizer st;
@@ -72,17 +40,36 @@ public class Backjoon6549 {
         while(true){
             st = new StringTokenizer(br.readLine(), " ");
             n = Integer.parseInt(st.nextToken());
-            arr = new int[n];
+            arr = new long[n];
             if(n == 0) break;
 
-            for(int i = 0 ; i < n; i++)
-                arr[i] = Integer.parseInt(st.nextToken());
+            for(int i = 0 ; i < n ; i++)
+                arr[i] = Long.parseLong(st.nextToken());
 
-            SegmentTree segmentTree = new SegmentTree(arr);
-            bw.write(segmentTree.getMaxWidth(0, n - 1) + "\n");
+            Stack<Info> stack = new Stack<>();
+            long area = 0;
+            for(int i = 0 ; i < n; i++){
+                int idx = i;
+
+                while(!stack.isEmpty() && arr[i] <= arr[stack.peek().getIdx()]){
+                    long tempArea = stack.peek().height * (i - stack.peek().getIdx());
+                    area = Math.max(tempArea, area);
+                    idx = stack.peek().getIdx();
+                    stack.pop();
+                    System.out.println(area);
+                }
+                stack.push(new Info(i, arr[i]));
+            }
+
+            while(!stack.isEmpty()){
+                long tempArea = stack.peek().height * (n - stack.peek().getIdx());
+                area = Math.max(tempArea, area);
+                stack.pop();
+            }
+
+            System.out.println(area);
+
         }
-        bw.close();
         br.close();
     }
-
 }
