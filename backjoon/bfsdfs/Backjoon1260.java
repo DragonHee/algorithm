3a32d2d6cf6fdcc8a297fded820618423e3d6d9b
kimyonghee <yhk06262@naver.com>
2020-01-30 20:03:33 +0900

    # 주석 추가

--- backjoon/bfsdfs/Backjoon1260.java
+++ backjoon/bfsdfs/Backjoon1260.java
@@ -23,6 +23,7 @@ public class Backjoon1260 {
         arr = new int[n + 1][n + 1];
         check = new boolean[n + 1];
 
+        // 노드와 간선에 대한 값을 초기화
         for(int i = 0 ; i < m; i++){
             st= new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
@@ -38,24 +39,32 @@ public class Backjoon1260 {
         System.out.println(sb);
     }
 
+    // check배열 false로 초기화
     public static void initCheck(){
         for(int i = 0 ; i < check.length; i++) check[i] = false;
     }
 
+    //dfs 메소드
     public static void dfs(int start){
+        // 경로 출력
         sb.append(start + " ");
+        // 현재 노드 방문 처리
         check[start] = true;
 
         for(int i = 1; i < check.length; i++)
+            // 현재 노드와 다른 노드 중 && 미방문 && 간선이 존재
             if(i != start && check[i] == false && arr[start][i] == 1)
                 dfs(i);
 
     }
     public static void bfs(int start){
+        // check배열 초기화
         initCheck();
         Queue<Integer> queue = new LinkedList<>();
 
+        // 처음 시작노드 추가
         queue.add(start);
+        // 처음 시작노드 방문처리
         check[start] = true;
 
         while(!queue.isEmpty()){
@@ -63,7 +72,9 @@ public class Backjoon1260 {
             sb.append(num + " ");
 
             for(int i = 1; i < check.length; i++){
+                // 현재 노드와 다른 노드 중 && 미방문 && 간선이 존재
                 if(i != num && check[i] == false && arr[num][i] == 1) {
+                    // 노드 추가
                     queue.add(i);
                     check[i] = true;
                 }
