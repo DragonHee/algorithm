d9ad2eeaef389ece2cc20cc34a4e1a0a56b8abe6
kimyonghee <yhk06262@naver.com>
2020-01-30 20:15:47 +0900

    # 주석 추가

--- backjoon/bfsdfs/Backjoon2606.java
+++ backjoon/bfsdfs/Backjoon2606.java
@@ -17,29 +17,38 @@ public class Backjoon2606 {
         boolean[][] isEdge = new boolean[n + 1][n + 1];
         boolean[] check = new boolean[n + 1];
 
+        // 간선정보 초기화
         for(int i = 1; i <= m; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             int nodeA = Integer.parseInt(st.nextToken());
             int nodeB = Integer.parseInt(st.nextToken());
 
+            // (1,2)가 간선이라면 (2,1)도 간선이다.
             isEdge[nodeA][nodeB] = isEdge[nodeB][nodeA] = true;
         }
 
         solve(isEdge, check, n);
     }
+
+    // bfs로 풀이한다.
     public static void solve(boolean[][] isEdge, boolean[] check, int n){
         Queue<Integer> queue = new LinkedList<>();
+        // 시작점 큐에 추가
         queue.add(1);
         check[1] = true;
         int ans = 0;
 
         while(!queue.isEmpty()){
+            // 노드를 하나 꺼낸다.
             int num = queue.poll();
+            // 갈수 있는 노드의 개수를 증가시킨다.
             ans++;
 
             for(int i = 1; i <= n; i++){
+                // 현재 노드와 다른 노드 && 간선 존재 && 미방문한 노드
                 if(i != num && isEdge[num][i] == true && check[i] == false){
                     queue.add(i);
+                    // 방문 노드로 만듬
                     check[i] = true;
                 }
             }
