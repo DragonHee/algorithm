875385ba9df39afe828fdd857c0306d3b30b2706
kimyonghee <yhk06262@naver.com>
2020-02-04 12:57:44 +0900

    # 최단거리 + 가중치 -> 다익스트라 / 폴노이만
    - 이 문제는 다익스트라 + PriorityQueue로 풀이하였다.

--- backjoon/minpath/Backjoon1753.java
+++ backjoon/minpath/Backjoon1753.java
@@ -1,23 +1,27 @@
 package backjoon.minpath;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
-class Node{
+class Node implements Comparable<Node>{
     int end, weight;
+
     public Node(int end, int weight){
         this.end = end;
         this.weight = weight;
     }
+
+    @Override
+    public int compareTo(Node o) {
+        return weight - o.weight;
+    }
 }
 public class Backjoon1753 {
     private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     private static final int INF = 100_000_000;
     static int v,e,k;
     static List<Node>[] list;
-    static boolean[] check;
     static int[] dist;
 
 
@@ -28,63 +32,55 @@ public class Backjoon1753 {
         k = Integer.parseInt(br.readLine());
         list = new ArrayList[v + 1];
         dist = new int[v + 1];
-        check = new boolean[v + 1];
 
         Arrays.fill(dist, INF);
 
         for(int i = 1; i <= v; i++){
             list[i] = new ArrayList<>();
         }
-
+        // 리스트에 그래프 정보를 초기화
         for(int i = 0 ; i < e; i++){
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
             int weight = Integer.parseInt(st.nextToken());
-
+            // start에서 end로 가는 weight 가중치
             list[start].add(new Node(end, weight));
         }
 
         StringBuilder sb = new StringBuilder();
+        // 다익스트라 알고리즘
         dijkstra(k);
-
+        // 출력 부분
         for(int i = 1; i <= v; i++){
             if(dist[i] == INF) sb.append("INF\n");
             else sb.append(dist[i] + "\n");
         }
-        System.out.println(sb);
+
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
     }
 
-    private static void dijkstra(int startNode){
-       dist[startNode] = 0;
+    private static void dijkstra(int start){
+       PriorityQueue<Node> queue = new PriorityQueue<>();
+       boolean[] check = new boolean[v + 1];
+       queue.add(new Node(start, 0));
+       dist[start] = 0;
 
-       while(true){
-           int minIndex = getMinNodeIndex();
-           if(minIndex == -1) break;
-           check[minIndex] = true;
+       while(!queue.isEmpty()){
+           Node curNode = queue.poll();
+           int cur = curNode.end;
 
-           for(int i = 0; i < list[minIndex].size(); i++){
-               int nextNode = list[minIndex].get(i).end;
-               int weight = list[minIndex].get(i).weight;
+           if(check[cur] == true) continue;
+           check[cur] = true;
 
-               if(dist[nextNode] > dist[minIndex] + weight){
-                   dist[nextNode] = dist[minIndex] + weight;
+           for(Node node : list[cur]){
+               if(dist[node.end] > dist[cur] + node.weight){
+                   dist[node.end] = dist[cur] + node.weight;
+                   queue.add(new Node(node.end, dist[node.end]));
                }
            }
        }
-
     }
-    private static int getMinNodeIndex(){
-        int min = INF, minIndex = -1;
-
-        for(int i = 1; i <= v; i++){
-            if(check[i] == false && dist[i] < min) {
-                min = dist[i];
-                minIndex = i;
-            }
-        }
-
-        return minIndex;
-    }
-
 }
