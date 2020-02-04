68b1b972138ff130758cd77337dc22f4fed4c5a9
kimyonghee <yhk06262@naver.com>
2020-02-04 12:35:28 +0900

    # 최단거리 + 가중치 -> 다익스트라 / 폴노이만
    - 이 문제는 다익스트라로 풀이하였다.
    - 다익스트라 풀이 과정 중 배열을 통해 풀이하였다. -> PriorityQueue를 이용하면 더욱 빠르게 계산이 가능하다.
    - 그래프 정보를 배열로 선언하게 되면 메모리 초과가 발생한다.-> list로 대체할 수 있다.

--- backjoon/minpath/Backjoon1753.java
+++ backjoon/minpath/Backjoon1753.java
@@ -3,22 +3,38 @@ package backjoon.minpath;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
+class Node{
+    int end, weight;
+    public Node(int end, int weight){
+        this.end = end;
+        this.weight = weight;
+    }
+}
 public class Backjoon1753 {
     private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final int INF = 100_000_000;
     static int v,e,k;
-    static int[][] arr;
-    static boolean[] visited;
-    static int answer;
+    static List<Node>[] list;
+    static boolean[] check;
+    static int[] dist;
+
 
     public static void main(String[] args) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
         v = Integer.parseInt(st.nextToken());
         e = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(br.readLine());
-        arr = new int[v + 1][v + 1];
-        visited = new boolean[v + 1];
+        list = new ArrayList[v + 1];
+        dist = new int[v + 1];
+        check = new boolean[v + 1];
+
+        Arrays.fill(dist, INF);
+
+        for(int i = 1; i <= v; i++){
+            list[i] = new ArrayList<>();
+        }
 
         for(int i = 0 ; i < e; i++){
             st = new StringTokenizer(br.readLine());
@@ -26,37 +42,49 @@ public class Backjoon1753 {
             int end = Integer.parseInt(st.nextToken());
             int weight = Integer.parseInt(st.nextToken());
 
-            arr[start][end] = weight;
+            list[start].add(new Node(end, weight));
         }
 
         StringBuilder sb = new StringBuilder();
+        dijkstra(k);
 
         for(int i = 1; i <= v; i++){
-            init();
-            answer = Integer.MAX_VALUE;
-            visited[k] = true;
-            dfs(k, i, 0);
-            if(answer == Integer.MAX_VALUE) sb.append("INF\n");
-            else sb.append(answer + "\n");
+            if(dist[i] == INF) sb.append("INF\n");
+            else sb.append(dist[i] + "\n");
         }
         System.out.println(sb);
     }
-    private static void init(){
-        for(int i = 1; i <= v; i++) visited[i] = false;
-    }
 
-    private static void dfs(int curNode, int destNode, int weight) {
-        if(curNode == destNode) {
-            answer = Math.min(answer, weight);
-            return;
-        }
+    private static void dijkstra(int startNode){
+       dist[startNode] = 0;
+
+       while(true){
+           int minIndex = getMinNodeIndex();
+           if(minIndex == -1) break;
+           check[minIndex] = true;
+
+           for(int i = 0; i < list[minIndex].size(); i++){
+               int nextNode = list[minIndex].get(i).end;
+               int weight = list[minIndex].get(i).weight;
+
+               if(dist[nextNode] > dist[minIndex] + weight){
+                   dist[nextNode] = dist[minIndex] + weight;
+               }
+           }
+       }
+
+    }
+    private static int getMinNodeIndex(){
+        int min = INF, minIndex = -1;
 
         for(int i = 1; i <= v; i++){
-            if(arr[curNode][i] > 0 && !visited[i]) {
-                visited[i] = true;
-                dfs(i, destNode, weight + arr[curNode][i]);
-                visited[i] = false;
+            if(check[i] == false && dist[i] < min) {
+                min = dist[i];
+                minIndex = i;
             }
         }
+
+        return minIndex;
     }
+
 }
