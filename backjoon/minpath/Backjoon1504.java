7bbe4672efcc69b94998dbf15e36d7be5d4e53b4
kimyonghee <yhk06262@naver.com>
2020-02-05 21:00:40 +0900

    #

---
+++ backjoon/minpath/Backjoon1504.java
@@ -0,0 +1,18 @@
+package backjoon.minpath;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Backjoon1504 {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    private static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
+
+
+
+    }
+}
