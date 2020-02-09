bacbc98c1c291f349ee061434ce2d4a312cc2d9b
kimyonghee <yhk06262@naver.com>
2020-02-09 16:22:17 +0900

    # 벨만 포드 알고리즘
    - 음의 가중치가 있기때문에 벨만 포드를 사용하여야한다.
    - v - 1번  * 간선의 개수 만큼 반복하며 갱신을 하고
    - 그 이후에도 갱신이 된다면 음의 사이클이 있다고 판단한다.
    
    * 이 문제에서는 무한대를 50_000_000으로 설정했다면
    음의 가중치가 있을 때 무한 > 무한 - 음의 가중치 를 하면 조건이 성립해버리게 된다.
    왜냐하면 거리가 존재하지 않는다는 뜻인데 존재하는 것처럼 계산이 되는 것이다.
    그러므로 dist[i] != INF와 같은 조건도 걸어줘야만 한다.

--- backjoon/minpath/Backjoon11657.java
+++ backjoon/minpath/Backjoon11657.java
@@ -1,71 +1,89 @@
 package backjoon.minpath;
 
+
 import java.io.*;
-import java.util.Arrays;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class Backjoon11657 {
-    static class Node implements Comparable<Node>{
-        int end, weight;
-        public Node(int end, int weight){this.end = end; this.weight = weight;}
+class Bus{
+    // 시작점, 도착점, 걸리는 시간
+    int start, end, weight;
 
-        @Override
-        public int compareTo(Node o) {
-            return weight - o.weight;
-        }
+    public Bus(int start, int end, int weight){
+        this.start = start;
+        this.end = end;
+        this.weight = weight;
     }
+}
+
+public class Backjoon11657 {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    private static final int INF = 500 * 10_000;
+    private static final int INF = 500 * 100_000;
+    static int n, m;
+    static int dist[];
+    static Bus busArr[];
 
     public static void main(String[] args) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        int arr[][] = new int[n + 1][n + 1];
-        int dist[] = new int[n + 1];
-        boolean check[] = new boolean[n + 1];
+        busArr = new Bus[m + 1];
+        dist = new int[n + 1];
+        // 최단거리 배열 무한대로 초기화
+        for(int i = 1 ; i <= n; i++) dist[i] = INF;
 
-        for(int i = 0 ; i <= n; i++){
-            Arrays.fill(arr[i], INF);
-            dist[i] = INF;
-        }
-
+        // 입력값 초기화
         for(int i = 0; i < m; i++){
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
             int time = Integer.parseInt(st.nextToken());
 
-            arr[start][end] = time;
+            busArr[i] = new Bus(start, end, time);
+        }
+        // 결과 출력을 위한 객체
+        StringBuilder sb = new StringBuilder();
+
+        // 음의 cycle이 없는 경우
+        if(bellmanFord()){
+            for(int i = 2; i <= n; i++){
+                sb.append(dist[i] == INF ? "-1\n" : dist[i] + "\n");
+            }
+        // 음의 cycle이 있는 경우
+        }else{
+            sb.append("-1\n");
         }
 
-        PriorityQueue<Node> queue = new PriorityQueue<>();
-        queue.add(new Node(1, 0));
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
+    }
+    // 음의 cycle이 있다면 false리턴 없다면 true리턴
+    // 벨만포드 알고리즘
+    private static boolean bellmanFord() {
+        // 시작점 최단거리 0 갱신
         dist[1] = 0;
 
-        while(!queue.isEmpty()){
-            Node curNode = queue.poll();
-            int cur = curNode.end;
-            int weight = curNode.weight;
-
-            for(int i = 1; i <= n; i++){
-                if(dist[i] > weight + arr[cur][i]){
-                    dist[i] = weight + arr[cur][i];
-                    queue.add(new Node(i, dist[i]));
+        // v - 1번 수행
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j < m; j++){
+                Bus bus = busArr[j];
+                // 더 작은 최단거리 가 있는 경우 갱신
+                if(dist[bus.start] != INF && dist[bus.end] > dist[bus.start] + bus.weight){
+                    dist[bus.end] = dist[bus.start] + bus.weight;
                 }
             }
         }
 
-        for(int i = 2; i <= n; i++) {
-            if (dist[i] < 0 || dist[i] == INF) bw.write("-1\n");
-            else bw.write(dist[i] + "\n");
+        // 음수 cycle 확인
+        // 갱신되는게 있다면 음수 cycle이 있다는 뜻
+        for(int i = 0; i < m; i++){
+            Bus bus = busArr[i];
+
+            if(dist[bus.start] != INF &&dist[bus.end] > dist[bus.start] + bus.weight) return false;
         }
 
-        bw.close();
-        br.close();
-
+        return true;
     }
 }
