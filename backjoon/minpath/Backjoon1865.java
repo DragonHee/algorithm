e4c2ff07162e65ba69845679e4cde0e1bf426ebc
kimyonghee <yhk06262@naver.com>
2020-02-10 12:51:17 +0900

    # 주석추가

--- backjoon/minpath/Backjoon1865.java
+++ backjoon/minpath/Backjoon1865.java
@@ -5,9 +5,14 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Backjoon1865 {
+    // 길 정보를 저장하기 위한 클래스
+    // static으로 선언한 이유는 단지 편리함을 위해서.
     static class Road{
+        // 출발노드
         int start;
+        // 도착노드
         int end;
+        // 걸리는 시간
         int cost;
 
         public Road(int start, int end, int cost){
@@ -23,17 +28,23 @@ public class Backjoon1865 {
     static int n, m, w;
     static Road[] roads;
     static int[] dist;
+
     public static void main(String[] args) throws IOException {
         int testCnt = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
 
+        // 테스트 개수만큼 반복
         for(int i = 0 ; i < testCnt; i++){
+            // 입력값을 변수에 초기화한다.
             init();
+            // 음수 사이클이 있는 경우
             if(bellmanFord())
                 sb.append("YES\n");
+            // 음수 사이클이 없는 경우
             else
                 sb.append("NO\n");
         }
+
         bw.write(sb.toString());
 
         br.close();
@@ -49,7 +60,7 @@ public class Backjoon1865 {
         dist = new int[n + 1];
         roads = new Road[2 * m + w];
 
-        Arrays.fill(dist, INF);
+        // roads[]의 인덱스를 나타낸다.
         int index = 0;
 
         for(int i = 0 ; i < m + w; i++){
@@ -58,9 +69,11 @@ public class Backjoon1865 {
             int end = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
 
+            // 무방향이기 때문에 반대방향도 추가한다.
             if(i < m){
                 roads[index++] = new Road(start, end, cost);
                 roads[index++] = new Road(end, start, cost);
+            // 웜홀은 유방향이기 때문에 시간만 * -1 해주어서 추가한다.
             }else{
                 roads[index++] = new Road(start, end, cost * -1);
             }
@@ -68,10 +81,14 @@ public class Backjoon1865 {
     }
 
     private static boolean bellmanFord(){
+        // dist배열을 INF로 초기화
         Arrays.fill(dist, INF);
+        // 시작점을 1번 노드로 지정하고 dist[1]값을 0으로 초기화
         dist[1] = 0;
 
+        // v - 1번 relax해준다.
         for (int i = 1; i < n; i++) {
+            // edge개수 만큼 확인을 한다.
             for (int j = 0; j < roads.length; j++) {
                 Road road = roads[j];
 
@@ -81,13 +98,15 @@ public class Backjoon1865 {
             }
         }
 
+        // 음수 사이클 유무 판정
         for(int i = 0 ; i < roads.length; i++){
             Road road = roads[i];
-
+            // 갱신이 또 된 경우
             if (dist[road.start] != INF && dist[road.start] + road.cost < dist[road.end])
                 return true;
         }
 
+        // 갱신이 되지 않고 for문을 빠져나온 경우
         return false;
     }
 }
