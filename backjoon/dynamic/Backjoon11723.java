b218a248c153ff2a2f309d210a4a90ac3b6c4037
kimyonghee <yhk06262@naver.com>
2020-02-11 12:45:58 +0900

    # 비트를 이용한 풀이법

--- backjoon/dynamic/Backjoon11723.java
+++ backjoon/dynamic/Backjoon11723.java
@@ -8,37 +8,43 @@ import java.util.StringTokenizer;
 public class Backjoon11723 {
     private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    // 출력을 위한 객체
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
-        int n = Integer.parseInt(br.readLine());
-        boolean arr[] = new boolean[21];
+       int n = Integer.parseInt(br.readLine());
+       int bitset = 0;
+
        while(n-- > 0){
-           StringTokenizer st = new StringTokenizer(br.readLine());
-           String op = st.nextToken();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String op = st.nextToken();
             int num;
+
+            // 수행연산
             switch (op){
                 case "add" :
                     num = Integer.parseInt(st.nextToken());
-                    arr[num] = true;
+                    // num - 1인 이유는 20이 들어왔을 때
+                    // 2^19 자리가 20번째이기 때문이다.
+                    bitset |= (1 << (num - 1));
                     break;
                 case "remove" :
                     num = Integer.parseInt(st.nextToken());
-                    arr[num] = false;
+                    bitset = bitset & ~(1 << (num - 1));
                     break;
                 case "check" :
                     num = Integer.parseInt(st.nextToken());
-                    sb.append(arr[num] == true ? "1\n" : "0\n");
+                    sb.append((bitset & (1 << (num - 1))) != 0 ? "1\n" : "0\n");
                     break;
                 case "toggle" :
                     num = Integer.parseInt(st.nextToken());
-                    arr[num] = !arr[num];
+                    bitset ^= (1 << (num - 1));
                     break;
                 case "all" :
-                    Arrays.fill(arr, true);
+                    bitset |= (~0);
                     break;
                 case "empty" :
-                    Arrays.fill(arr, false);
+                    bitset &= 0;
                     break;
             }
         }
