0f1fbe40411f0d2826b58b54e5f737d63e04ed2a
kimyonghee <yhk06262@naver.com>
2020-02-16 00:10:50 +0900

    # 코드 리팩토링

--- backjoon/dp_minpath/Backjoon12852.java
+++ backjoon/dp_minpath/Backjoon12852.java
@@ -1,16 +1,14 @@
 package backjoon.dp_minpath;
 
 import java.io.*;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class Backjoon12852 {
     private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         int n = Integer.parseInt(br.readLine());
-        int result;
         int dp[] = new int[n + 1];
 
         for(int i = 2; i <= n; i++){
@@ -25,38 +23,26 @@ public class Backjoon12852 {
             dp[i] = Math.min(num1, Math.min(num2, num3)) + 1;
         }
 
-        result = dp[n];
-        bw.write(result + "\n");
-        int num = result;
+       sb.append(dp[n] + "\n");
+
+        int num = dp[n];
         int index = n;
+
         while (true){
+            sb.append(index + " ");
+
             if(num == 0) break;
 
-            if(dp[index] == num){
-                bw.write(index + " ");
-                if(index % 3 == 0 && dp[index / 3] == num - 1){
-                    index = index / 3;
-                }else if(index % 2 == 0 && dp[index / 2] == num - 1) {
-                    index = index / 2;
-                    bw.write("\n");
-                }else{
-                    index = index - 1;
-                }
+            if(index % 3 == 0 && dp[index / 3] == num - 1)
+                index = index / 3;
+            else if(index % 2 == 0 && dp[index / 2] == num - 1)
+                index = index / 2;
+            else
+                index = index - 1;
 
-                num--;
-            }
-
-
-
+            num--;
         }
-        for(int i = 1; i <= n; i++){
-            System.out.print(i + "\t");
-        }
-        System.out.println();
-        for(int i = 1; i <= n; i++){
-            System.out.print(dp[i] + "\t");
-        }
-        bw.write(result + "\n");
+        bw.write(sb.toString());
 
         bw.close();
         br.close();
