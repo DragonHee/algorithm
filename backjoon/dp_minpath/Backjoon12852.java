fb1b1010866a680eda49ea4b4a44f643e99e80e0
kimyonghee <yhk06262@naver.com>
2020-02-19 10:22:52 +0900

    # 주석추가

--- backjoon/dp_minpath/Backjoon12852.java
+++ backjoon/dp_minpath/Backjoon12852.java
@@ -1,46 +1,62 @@
 package backjoon.dp_minpath;
 
 import java.io.*;
+import java.util.Arrays;
+import java.util.Stack;
 
 public class Backjoon12852 {
     private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    // 무한대의 값
+    private static final int INF = 1_000_000;
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         int n = Integer.parseInt(br.readLine());
         int dp[] = new int[n + 1];
 
-        for(int i = 2; i <= n; i++){
-            int num1 = Integer.MAX_VALUE;
-            int num2 = Integer.MAX_VALUE;
-            int num3;
+        Arrays.fill(dp, INF);
+        //n을 만들기 위해 걸리는 최소 시간 = 0
+        dp[n] = 0;
 
-            if(i % 3 == 0) num1 = dp[i / 3];
-            if(i % 2 == 0) num2 = dp[i / 2];
-            num3 = dp[i - 1];
+        for(int i = n; i >= 1; i--){
+           // 현재 i위치에서 가는 최솟값
+           int minValue = dp[i] + 1;
 
-            dp[i] = Math.min(num1, Math.min(num2, num3)) + 1;
+           if(i % 3 == 0) dp[i / 3] = Math.min(dp[i / 3], minValue);
+           if(i % 2 == 0) dp[i / 2] = Math.min(dp[i / 2], minValue);
+           dp[i - 1] = Math.min(dp[i - 1], minValue);
+
+
+        }
+        // 1까지 가는 최소 횟수
+        sb.append(dp[1] + "\n");
+
+        int minValue = dp[1];
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = 1; i <= n; i++){
+            if(minValue == dp[i]){
+                stack.push(i);
+
+                // 3배의 수의 dp값이 dp[i] - 1과 같은 경우
+                if(i * 3 <= n && dp[i * 3] == minValue - 1)
+                    // i값은 3배인 수로 변경
+                    i = i * 3 - 1;
+                // 2배의 수의 dp값이 dp[i] - 1과 같은 경우
+                else if(i * 2 <= n && dp[i * 2] == minValue - 1){
+                    i = i * 2 - 1;
+                }else if(i + 1 <= n && dp[i + 1] == minValue - 1){
+
+                }
+
+                minValue--;
+            }
         }
 
-       sb.append(dp[n] + "\n");
-
-        int num = dp[n];
-        int index = n;
-
-        while (true){
-            sb.append(index + " ");
-
-            if(num == 0) break;
-
-            if(index % 3 == 0 && dp[index / 3] == num - 1)
-                index = index / 3;
-            else if(index % 2 == 0 && dp[index / 2] == num - 1)
-                index = index / 2;
-            else
-                index = index - 1;
-
-            num--;
+        // 역추적한 결과 출력
+        while (!stack.isEmpty()){
+            sb.append(stack.pop() + " ");
         }
         bw.write(sb.toString());
 
