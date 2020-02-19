1c00d28b70171afe05e45a601f906371b37d1203
kimyonghee <yhk06262@naver.com>
2020-02-20 08:50:27 +0900

    #주석추가

--- backjoon/dp_minpath/Backjoon12738.java
+++ backjoon/dp_minpath/Backjoon12738.java
@@ -12,7 +12,9 @@ public class Backjoon12738 {
     public static void main(String[] args) throws IOException {
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
+        // 증가수열을 저장할 리스트
         List<Integer> list = new ArrayList<>();
+        // 입력된 값을 저장할 배열
         int arr[] = new int[n + 1];
 
         for(int i = 1 ; i <= n; i++) arr[i] = Integer.parseInt(st.nextToken());
@@ -24,10 +26,13 @@ public class Backjoon12738 {
             int left = 1;
             int right = list.size() - 1;
 
-            // 확인하는 숫자가 수열이 마지막 수보다 큰 경우
+            // 확인하는 숫자가 증가수열의 마지막 수보다 큰 경우
             // 수열에 추가해준다.
             if(num > list.get(list.size() - 1)) list.add(num);
+            // 확인하는 숫자가 증가수열의 마지막 수보다 작은 경우
             else{
+                // 숫자의 적당한 위치를 찾아
+                // 증가수열의 값을 변경해준다.
                 while(left < right){
                     int mid = (left + right) >> 1;
 
