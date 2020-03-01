cd9b3b948ec291cde1ebdc3590fe8285f636635d
kimyonghee <yhk06262@naver.com>
2020-03-01 15:36:52 +0900

    #lcm 알고리즘을 이용하여 길이를 구한다.
    -- 경로를 출력해야 하므로 길이를 구하는 동시에
    -- 경로를 같이 저장한다.

--- backjoon/dp_minpath/Backjoon9252.java
+++ backjoon/dp_minpath/Backjoon9252.java
@@ -1,43 +1,64 @@
 package backjoon.dp_minpath;
 
 import java.io.*;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
 
 // 사실 lcm알고리즘을 모르고 있다면
 // 생각해서 풀기 힘들기 때문에
 // 숙지하고 있도록 하자.
 
+class Point{
+    int length;
+    String seq;
+
+    public Point(){
+        this.length = 0;
+        this.seq = "";
+    }
+}
+
 public class Backjoon9252 {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringBuilder sb = new StringBuilder();
-    static int answer;
+    static int resultLength;
+    static String resultSeq;
 
     public static void main(String[] args) throws IOException {
         String str1 = br.readLine();
         String str2 = br.readLine();
 
-        int lcmArr[][] = new int[str1.length() + 1][str2.length() + 1];
+        Point lcmArr[][] = new Point[str1.length() + 1][str2.length() + 1];
 
-
-        for(int i = 0; i < str1.length(); i++){
-            boolean check1 = false;
-            for(int j = 0 ; j < str2.length(); j++){
-                if(str1.charAt(i) == str2.charAt(j)){
-                    lcmArr[i + 1][j + 1] = lcmArr[i][j] + 1;
-                }
-                else{
-                    lcmArr[i + 1][j + 1] = Math.max(lcmArr[i][j + 1], lcmArr[i + 1][j]);
-                }
-                answer = Math.max(answer, lcmArr[i + 1][j + 1]);
+        for(int i = 0 ; i <= str1.length(); i++){
+            for(int j = 0 ; j <= str2.length(); j++){
+                lcmArr[i][j] = new Point();
             }
         }
-        sb.append(answer + "\n");
 
+        for(int i = 0; i < str1.length(); i++){
+            for(int j = 0 ; j < str2.length(); j++){
+                if(str1.charAt(i) == str2.charAt(j)){
+                    lcmArr[i + 1][j + 1].length  = lcmArr[i][j].length + 1;
+                    lcmArr[i + 1][j + 1].seq = lcmArr[i][j].seq + str1.charAt(i);
+                }
+                else{
+                    if(lcmArr[i + 1][j].length >= lcmArr[i][j + 1].length){
+                        lcmArr[i + 1][j + 1].length  = lcmArr[i + 1][j].length;
+                        lcmArr[i + 1][j + 1].seq = lcmArr[i + 1][j].seq;
+                    }
+                    else{
+                        lcmArr[i + 1][j + 1].length  = lcmArr[i][j + 1].length;
+                        lcmArr[i + 1][j + 1].seq = lcmArr[i][j + 1].seq;
+                    }
+                }
 
-        sb.append("\n");
+                if(resultLength < lcmArr[i + 1][j + 1].length){
+                    resultLength = lcmArr[i + 1][j + 1].length;
+                    resultSeq = lcmArr[i + 1][j + 1].seq;
+                }
+            }
+        }
+        sb.append(resultLength + "\n" + resultSeq + "\n");
 
         bw.write(sb.toString());
         bw.close();
