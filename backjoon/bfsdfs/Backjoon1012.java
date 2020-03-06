9179d08b9c5f08c7dbb76dbed70435267fb0afbb
DragonHee <50479743+DragonHee@users.noreply.github.com>
2020-03-06 20:55:33 +0900

    Update Backjoon1012.java
    주석처리

--- backjoon/bfsdfs/Backjoon1012.java
+++ backjoon/bfsdfs/Backjoon1012.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+
 public class Backjoon1012 {
     static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static boolean arr[][];
