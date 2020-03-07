4e85c653162dd1681565d1dad9d01be370afe956
kimyonghee <yhk06262@naver.com>
2020-03-07 16:13:18 +0900

    # 인접 리스트로 연결정보를 저장한 뒤 bfs를 이용해서 경로를 모두 탐색하며 부모노드 정보를 저장한다.

--- backjoon/tree/Backjoon11725.java
+++ backjoon/tree/Backjoon11725.java
@@ -1,41 +1,57 @@
 package backjoon.tree;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Backjoon11725 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static final int INF = 100_000;
     static StringBuilder sb = new StringBuilder();
+    static List<Integer> list[];
 
     public static void main(String[] args) throws IOException {
         int N = Integer.parseInt(br.readLine());
 
-        boolean check[] = new boolean[INF + 1];
-        int parent[] = new int[INF + 1];
+        list = new ArrayList[N + 1];
+        int parent[] = new int[N + 1];
+        boolean isVisit[] = new boolean[N + 1];
 
-
-        check[1] = true;
-        parent[1] = 1;
-
+        for(int i = 1; i <= N; i++){
+            list[i] = new ArrayList<>();
+        }
 
         for(int i = 1; i < N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             int num1 = Integer.parseInt(st.nextToken());
             int num2 = Integer.parseInt(st.nextToken());
 
-            if(check[num1] == false){
-                parent[num1] = num2;
-                check[num1] = true;
-            }else{
-                parent[num2] = num1;
-                check[num2] = true;
+            list[num1].add(num2);
+            list[num2].add(num1);
+        }
+
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(1);
+        isVisit[1] = true;
+
+        while(!stack.isEmpty()){
+            int start = stack.pop();
+
+            for(int i = 0 ; i < list[start].size(); i++){
+                int end = list[start].get(i);
+
+                if(isVisit[end] == false) {
+                    parent[end] = start;
+                    stack.push(end);
+                    isVisit[end] = true;
+                }
             }
         }
 
-        for(int i = 2; i <= N; i++)
-            sb.append(parent[i] + "\n");
+        for(int i = 2; i <= N; i++) sb.append(parent[i] + "\n");
 
         bw.write(sb.toString());
         bw.close();
