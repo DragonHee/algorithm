61c43bb25baf3728e9cc3992ae9f078509326b01
kimyonghee <yhk06262@naver.com>
2020-03-14 16:35:51 +0900

    # 주석 추가

--- backjoon/tree/Backjoon11725.java
+++ backjoon/tree/Backjoon11725.java
@@ -23,20 +23,24 @@ public class Backjoon11725 {
             list[i] = new ArrayList<>();
         }
 
+        // 인접행렬 초기화
         for(int i = 1; i < N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             int num1 = Integer.parseInt(st.nextToken());
             int num2 = Integer.parseInt(st.nextToken());
 
+            // num1, num2 각각 이어져 있는 노드를 추가한다.
             list[num1].add(num2);
             list[num2].add(num1);
         }
 
         Stack<Integer> stack = new Stack<>();
 
+        // 시작 노드 추가
         stack.push(1);
         isVisit[1] = true;
 
+        // DFS
         while(!stack.isEmpty()){
             int start = stack.pop();
 
@@ -51,8 +55,8 @@ public class Backjoon11725 {
             }
         }
 
+        // 2번 노드 부터 부모노드를 출력한다.
         for(int i = 2; i <= N; i++) sb.append(parent[i] + "\n");
-
 
         bw.write(sb.toString());
         bw.close();
