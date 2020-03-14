53437fcc3d5aa4e446c74b3b4dc3d4d269d60388
kimyonghee <yhk06262@naver.com>
2020-03-14 15:50:55 +0900

    # 주석 추가 및 리팩토링
    기존 : 경로 출력 시 하나하나 어떤 연산인지 검사
    수정 : 최단거리를 구할 때 배열에 연산 저장

--- backjoon/dp_minpath/Backjoon9019.java
+++ backjoon/dp_minpath/Backjoon9019.java
@@ -11,22 +11,35 @@ public class Backjoon9019 {
     static final int MAX = 10000;
     static boolean visited[];
     static int parent[];
+    static char register[];
+
+    // 결과 값 출력을 위한 객체 선언
+    static StringBuilder sb;
 
     public static void main(String[] args) throws IOException {
         int T = Integer.parseInt(br.readLine());
 
         while(T-- > 0){
+            sb = new StringBuilder();
             StringTokenizer st = new StringTokenizer(br.readLine());
 
+            // 입력 값 처리
             int A = Integer.parseInt(st.nextToken());
             int B = Integer.parseInt(st.nextToken());
 
+            // 미방문인 경우 false
+            // 방문인 경우 true
             visited = new boolean[MAX];
+            // 경로를 찾기 위해 부모노드의 값을 저장한다.
             parent = new int[MAX];
+            // 어떤 연산을 사용했는지를 저장한다.
+            register = new char[MAX];
 
-            StringBuilder sb = new StringBuilder();
+            // BFS를 위한 큐 선언
             Queue<Integer> queue = new LinkedList<>();
+            // 시작 노드를 큐에 추가
             queue.add(A);
+            // 시작 노드를 방문 처리한다.
             visited[A] = true;
 
             while(true){
@@ -41,61 +54,59 @@ public class Backjoon9019 {
                 int R = cur / 10 + lsb * 1000;
 
                 // D function
+                // D 명령을 한 값이 미방문인 경우
                 if(visited[D] == false){
                     visited[D] = true;
                     parent[D] = cur;
+                    register[D] = 'D';
                     queue.add(D);
                 }
 
                 // S function
+                // S 명령을 한 값이 미방문인 경우
                 if(visited[S] == false){
                     visited[S] = true;
                     parent[S] = cur;
+                    register[S] = 'S';
                     queue.add(S);
                 }
 
                 // L function
+                // L 명령을 한 값이 미방문인 경우
                 if(visited[L] == false){
                     visited[L] = true;
                     parent[L] = cur;
+                    register[L] = 'L';
                     queue.add(L);
                 }
 
                 // R function
+                // R 명령을 한 값이 미방문인 경우
                 if(visited[R] == false){
                     visited[R] = true;
                     parent[R] = cur;
+                    register[R] = 'R';
                     queue.add(R);
                 }
 
-                if(visited[B] != false) {
+                // 도달하고자 하는 노드가 방문인 경우
+                // 경로를 찾는다.
+                if(visited[B] == true) {
                     int curValue = B;
 
-                    while(true){
+                    while(curValue != A){
                         int parentValue = parent[curValue];
 
-                        int pMsb = parentValue / 1000;
-                        int pLsb = parentValue % 10;
-
-                        int PD = (parentValue * 2) % MAX;
-                        int PS = parentValue == 0 ? MAX - 1: parentValue - 1;
-                        int PL = (parentValue * 10 + pMsb) % MAX;
-                        int PR = parentValue / 10 + pLsb * 1000;
-
-                        if(PD == curValue) sb.append('D');
-                        else if(PS == curValue) sb.append('S');
-                        else if(PL == curValue) sb.append('L');
-                        else if(PR == curValue) sb.append('R');
-
+                        sb.append(register[curValue]);
                         curValue = parentValue;
-
-                        if(curValue == A) break;
                     }
-
+                    // 역순으로 연산을 sb에 append했기 때문에
+                    // reverse하여 출력한다.
+                    bw.write(sb.reverse().append("\n").toString());
                     break;
                 }
             }
-            bw.write(sb.reverse().toString() + "\n");
+
         }
         bw.close();
         br.close();
