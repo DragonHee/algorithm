1c9446088ebba83dccb99004903e311bb792017a
kimyonghee <yhk06262@naver.com>
2020-03-15 17:49:20 +0900

    # bfs 를 이용하여 풀이한다.
    - 키 포인트는 1번 노드에서 가장 먼 노드를 찾고
    - 찾은 노드에서 가장 먼 노드까지의 거리를 구한다.

--- backjoon/tree/Backjoon1167.java
+++ backjoon/tree/Backjoon1167.java
@@ -3,32 +3,37 @@ package backjoon.tree;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
+
 class Node{
     int end;
-    int dis;
+    int weight;
 
-    public Node(int end, int dis){
+    public Node(int end, int weight){
         this.end = end;
-        this.dis = dis;
+        this.weight = weight;
     }
 }
+
 public class Backjoon1167 {
     private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static List<Node> listArr[];
+    static int dist[];
+    static boolean visited[];
+    static int V;
+    static int answer;
+
 
     public static void main(String[] args) throws IOException {
-        int V = Integer.parseInt(br.readLine());
-        List<Node> listArr[] = new ArrayList[V + 1];
+        V = Integer.parseInt(br.readLine());
+        listArr = new ArrayList[V + 1];
 
         for(int i = 0 ; i <= V; i++){
             listArr[i] = new ArrayList<>();
         }
 
-        while(V-- > 0){
+        for(int i = 1; i <= V; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-
             int start = Integer.parseInt(st.nextToken());
 
             while(true){
@@ -38,8 +43,49 @@ public class Backjoon1167 {
 
                 listArr[start].add(new Node(end, distance));
                 listArr[end].add(new Node(start, distance));
+            }
+        }
 
+        int farestNode = 1;
 
+        bfs(farestNode);
+
+        for(int i = 1; i <= V; i++){
+            if(answer < dist[i]) {
+                answer = dist[i];
+                farestNode = i;
+            }
+        }
+
+        answer = 0;
+        bfs(farestNode);
+
+        for(int i = 1; i <= V; i++){
+            if(answer < dist[i]) {
+                answer = dist[i];
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+    private static void bfs(int node){
+        dist = new int[V + 1];
+        visited = new boolean[V + 1];
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(new Node(node, 0));
+        visited[node] = true;
+
+        while(!queue.isEmpty()){
+            Node curNode = queue.poll();
+
+            for(Node endNode : listArr[curNode.end]){
+                if(visited[endNode.end] == false){
+                    dist[endNode.end] = curNode.weight + endNode.weight;
+                    visited[endNode.end] = true;
+                    queue.add(new Node(endNode.end, dist[endNode.end]));
+                }
             }
         }
     }
