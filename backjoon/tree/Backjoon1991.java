e0de751daa4d75c784a78e12001617f0a983ff9e
kimyonghee <yhk06262@naver.com>
2020-03-20 20:58:25 +0900

    # 기본 전위, 중위, 후위 탐색

--- backjoon/tree/Backjoon1991.java
+++ backjoon/tree/Backjoon1991.java
@@ -3,27 +3,29 @@ package backjoon.tree;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Backjoon1991 {
     static class Node{
-        int me;
-        int left;
-        int right;
-        public Node(int me, int left, int right){
-            this.me = me;
-            this.left = left;
-            this.right = right;
+        int parent;
+        int leftChild;
+        int rightChild;
+        public Node(int parent, int leftChild, int rightChild){
+            this.parent = parent;
+            this.leftChild = leftChild;
+            this.rightChild = rightChild;
         }
     }
     static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         int N = Integer.parseInt(br.readLine());
 
 
         Node arr[] = new Node[N];
+        int parentArr[] = new int[N];
+        Arrays.fill(parentArr, -1);
 
         for(int i = 0 ; i < N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
@@ -36,26 +38,45 @@ public class Backjoon1991 {
             if(right == '.') right = '0';
 
             arr[parent - 'A'] = new Node(parent - 'A', left - 'A', right - 'A');
+            if(left != '0') parentArr[left - 'A'] = parent - 'A';
+            if(right != '0') parentArr[right - 'A'] = parent - 'A';
+
         }
 
         Stack<Node> stack = new Stack<>();
+        Queue<Integer> stackAns = new LinkedList<>();
+
         stack.push(arr[0]);
 
         while(!stack.isEmpty()){
             Node node = stack.pop();
 
-            if(node.left >= 0){
-                stack.push(arr[node.left]);
-            }
-            if(node.right >= 0){
-                stack.push(arr[node.right]);
-            }
-            System.out.print(node.me);
+            stackAns.add(node.parent);
+            if(node.rightChild >= 0) stack.push(arr[node.rightChild]);
+            if(node.leftChild >= 0) stack.push(arr[node.leftChild]);
         }
 
+        while (!stackAns.isEmpty()){
+            sb.append((char)(stackAns.poll() + 'A'));
+        }
+
+        stack.push(arr[0]);
+
+        while(!stack.isEmpty()){
+            Node node = stack.pop();
+
+            stackAns.add(node.parent);
+            if(node.rightChild >= 0) stack.push(arr[node.rightChild]);
+            if(node.leftChild >= 0) stack.push(arr[node.leftChild]);
+        }
+
+        while (!stackAns.isEmpty()){
+            sb.append((char)(stackAns.poll() + 'A'));
+        }
+
+
+        sb.append("\n");
+        System.out.println(sb.toString());
     }
 
-    public static void search1(){
-
-    }
 }
