847edaa94b1198e785f0d96d05d58241dd33d630
kimyonghee <yhk06262@naver.com>
2020-10-27 00:20:30 +0900

    if the input value under 2 it would get some errors, so i fixed the logic

--- backjoon/dynamic/Backjoon1904.java
+++ backjoon/dynamic/Backjoon1904.java
@@ -10,14 +10,14 @@ public class Backjoon1904 {
     public static void main(String[] args) throws IOException {
         int n = Integer.parseInt(br.readLine());
         tileArr = new int[n + 1];
+        tileArr[0] = 1;
         tileArr[1] = 1;
-        tileArr[2] = 2;
         bw.write(solve(n)  + "\n");
         bw.close();
         br.close();
     }
     private static int solve(int n){
-        for(int i = 3; i <= n; i++){
+        for(int i = 2; i <= n; i++){
             tileArr[i] = (tileArr[i - 1] + tileArr[i - 2]) % 15746;
         }
         return tileArr[n];
