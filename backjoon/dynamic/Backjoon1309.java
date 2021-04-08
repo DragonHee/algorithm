71a16cbedcd1a86681897de4d8a77c27bb76d979
kimyonghee <yhk06262@naver.com>
2021-04-08 16:49:48 +0900

    dynamic programming

--- backjoon/dynamic/Backjoon1309.java
+++ backjoon/dynamic/Backjoon1309.java
@@ -2,12 +2,12 @@ package backjoon.dynamic;
 
 import java.io.*;
 
-public class Backjoon11727 {
+public class Backjoon1309 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static int input;
-    static int dp[];
-
+    static int[] dp;
+    static final int DIVIDE_NUM = 9901;
 
     public static void main(String[] args) throws IOException{
         input = Integer.parseInt(br.readLine());
@@ -15,15 +15,16 @@ public class Backjoon11727 {
         dp = new int[input + 1];
         
         dp[0] = 1;
-        dp[1] = 1;
+        dp[1] = 3;
 
         for(int i = 2; i <= input; i++){
-            dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007;
+            dp[i] = (2 * dp[i - 1] + dp[i - 2]) % DIVIDE_NUM;
         }
 
         bw.write(dp[input] + "\n");
 
         bw.close();
         br.close();
-    }   
+    }
+    
 }
