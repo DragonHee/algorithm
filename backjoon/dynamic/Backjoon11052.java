abf9bd78e78a9fb38759d8a41376324fd5629d47
kimyonghee <yhk06262@naver.com>
2021-07-28 13:30:30 +0900

    dynamic programming 최선의 순간의 연속

---
+++ backjoon/dynamic/Backjoon11052.java
@@ -0,0 +1,56 @@
+package backjoon.dynamic;
+
+import java.util.*;
+import java.io.*;
+
+public class Backjoon11052 {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int totalCardCnt;
+    static int[] cardCostArr;
+
+    public static void main(String[] args) throws IOException {
+        totalCardCnt = Integer.parseInt(br.readLine());
+        cardCostArr = new int[totalCardCnt + 1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int i = 1; i <= totalCardCnt; i++){
+            cardCostArr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int answer = solve();
+
+        bw.write(answer + "\n");
+
+        bw.close();
+        br.close();        
+    }
+
+    public static int solve(){
+        int[][] dp = new int[totalCardCnt + 1][totalCardCnt + 1];
+        int maxValue = Integer.MIN_VALUE;
+
+        for(int i = 1; i <= totalCardCnt; i++){
+            for(int j = 1; j <= totalCardCnt; j++){
+                if(j - i < 0) {
+                    dp[i][j] = dp[i - 1][j];
+
+                }
+                else{
+                    dp[i][j] = Math.max(Math.max(dp[i - 1][j - i], dp[i][j - i]) + cardCostArr[i], dp[i - 1][j]);
+                    maxValue = Math.max(dp[i][j], maxValue);
+                }
+            }
+        }
+
+        for(int i = 0 ; i <= totalCardCnt; i++){
+            for(int j = 0 ; j <= totalCardCnt; j++){
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        return maxValue;
+    }
+}
